8194617e3960e3787843b5a0bdbf28436e1ab4fe
blavkice <beppespqr@gmail.com>
2025-05-10 18:05:40 +0200

    #18: implemented conversion for 1-100

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -14,12 +14,12 @@ public class IntegerToRoman {
      * @return the string representation of the roman number
      */
     public static String convert(int number) {
-        if (number <= 0 || number > 50) {
+        if (number <= 0 || number > 100) {
             throw new IllegalArgumentException("number should be > 0 and <= 6");
         }
 
-        int[] values = {50, 40, 10, 9, 5, 4, 1};
-        String[] symbols = {"L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder result = new StringBuilder();
 
