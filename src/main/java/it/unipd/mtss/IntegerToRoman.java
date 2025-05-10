8b98370084c5a09e636def4b0c6d143d7ea52a9c
Matteo Mantoan <mattemant@gmail.com>
2025-05-10 17:30:25 +0200

    close #15: convert first 10 numbers

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -14,12 +14,12 @@ public class IntegerToRoman {
      * @return the string representation of the roman number
      */
     public static String convert(int number) {
-        if (number <= 0 || number > 6) {
+        if (number <= 0 || number > 10) {
             throw new IllegalArgumentException("number should be > 0 and <= 6");
         }
 
-        int[] values = {5, 4, 1};
-        String[] symbols = {"V", "IV", "I"};
+        int[] values = {10, 9, 5, 4, 1};
+        String[] symbols = {"X", "IX", "V", "IV", "I"};
 
         StringBuilder result = new StringBuilder();
 
